2ffb6c6c3d7188d226adac3188015383eb7e57ce
immujahidkhan6@gmail.com <Password@2018>
2018-11-29 12:00:06 +0500

    init

--- app/src/main/java/com/justclack/agecalculator/MainActivity.java
+++ app/src/main/java/com/justclack/agecalculator/MainActivity.java
@@ -1,30 +1,37 @@
 package com.justclack.agecalculator;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
     Calendar myCalendar;
     Button btn;
+    EditText dob;
+    DatePickerDialog.OnDateSetListener date;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialiaze();
 
-        myCalendar = Calendar.getInstance();
-
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
+        date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
@@ -37,23 +44,51 @@ public class MainActivity extends Activity {
 
         };
         // Get the widgets reference from XML layout
-        btn = findViewById(R.id.btn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(this);
+        dob.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                new DatePickerDialog(MainActivity.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (dob.getRight() - dob.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // your action here
+                        //Toast.makeText(MainActivity.this, "Click on Drawable", Toast.LENGTH_SHORT).show();
+                        new DatePickerDialog(MainActivity.this, date, myCalendar
+                                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                        return true;
+                    }
+                }
+                return false;
             }
         });
 
+
+    }
+
+    private void initialiaze() {
+        btn = findViewById(R.id.btn);
+        dob = findViewById(R.id.dob);
+        myCalendar = Calendar.getInstance();
     }
 
     private void updateLabel() {
-        String myFormat = "MM/dd/yy"; //In which you need put here
+        String myFormat = "MM/dd/yyyy"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         btn.setText(sdf.format(myCalendar.getTime()));
+        dob.setText(sdf.format(myCalendar.getTime()));
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btn) {
+            new DatePickerDialog(MainActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    }
 }
